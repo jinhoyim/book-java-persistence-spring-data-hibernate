12d3b711b27596ac51e7c000d9a81915f4288a43
Jinho Yim <jinhoyim@outlook.com>
2024-12-01 22:33:27 +0900

    Hibernate 구성으로 EntityManagerFactory 생성하는 예

--- Ch02/helloworld/src/test/java/book/jpa/jpa_hibernate/ch02/HelloWorldJPATest.java
+++ Ch02/helloworld/src/test/java/book/jpa/jpa_hibernate/ch02/HelloWorldJPATest.java
@@ -3,8 +3,10 @@ package book.jpa.jpa_hibernate.ch02;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +14,34 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloWorldJPATest {
-    @Test
-    void storeLoadMessage() {
+
+    private static EntityManagerFactory createEntityManagerFactory() {
         Map<String, String> properties = new HashMap<>();
         properties.put("jakarta.persistence.jdbc.password", System.getenv("LOCAL_DEVDB_SUPER_PASSWORD"));
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ch02", properties);
+        return emf;
+    }
+
+    private static EntityManagerFactory createWithHibernateProperties() {
+        Configuration configuration = new Configuration();
+        configuration.getProperties().put("jakarta.persistence.jdbc.password", System.getenv("LOCAL_DEVDB_SUPER_PASSWORD"));
+        configuration.configure().addAnnotatedClass(Message.class);
+
+        Map<String, String> properties = new HashMap<>();
+        Enumeration<?> propertyNames = configuration.getProperties().propertyNames();
+
+        while (propertyNames.hasMoreElements()) {
+            String propertyName = (String)propertyNames.nextElement();
+            properties.put(propertyName, configuration.getProperties().getProperty(propertyName));
+        }
+
+        return Persistence.createEntityManagerFactory("ch02", properties);
+    }
+
+    @Test
+    void storeLoadMessage() {
+        EntityManagerFactory emf = createEntityManagerFactory();
 
         try {
             EntityManager em = emf.createEntityManager();
