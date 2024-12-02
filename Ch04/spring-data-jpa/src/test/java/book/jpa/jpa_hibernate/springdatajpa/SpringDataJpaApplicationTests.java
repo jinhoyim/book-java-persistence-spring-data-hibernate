e2d59df0500f71f1da614ee863f6f1c697df5ce7
Jinho Yim <jinhoyim@outlook.com>
2024-12-02 19:13:03 +0900

    Ch04 스프링 부트 프로젝트 추가

---
+++ Ch04/spring-data-jpa/src/test/java/book/jpa/jpa_hibernate/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package book.jpa.jpa_hibernate.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
