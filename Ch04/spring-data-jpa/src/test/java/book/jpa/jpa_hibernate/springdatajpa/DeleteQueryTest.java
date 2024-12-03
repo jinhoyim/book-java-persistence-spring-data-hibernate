34ef630b97cb089ea564404c7070dfc9d7dce8d8
Jinho Yim <jinhoyim@outlook.com>
2024-12-03 12:53:54 +0900

    UserRepository 수정/삭제 쿼리
    엔티티 목록을 조회 후 하나씩 삭제하는 방법을 회피하는 방법을 설명한다.

---
+++ Ch04/spring-data-jpa/src/test/java/book/jpa/jpa_hibernate/springdatajpa/DeleteQueryTest.java
@@ -0,0 +1,26 @@
+package book.jpa.jpa_hibernate.springdatajpa;
+
+import book.jpa.jpa_hibernate.springdatajpa.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeleteQueryTest extends SpringDataJpaApplicationTests {
+
+    @Test
+    void testDeleteByLevel() {
+        int deleted = userRepository.deleteByLevel(2);
+        List<User> users = userRepository.findByLevel(2, Sort.by("username"));
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    void testDeleteBulkByLevel() {
+        int deleted = userRepository.deleteBulkByLevel(2);
+        List<User> users = userRepository.findByLevel(2, Sort.by("username"));
+        assertEquals(0, users.size());
+    }
+}
