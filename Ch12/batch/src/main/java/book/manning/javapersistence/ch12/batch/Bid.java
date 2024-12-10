c7a08cfe76a178e96ac95c32fba221b3c98d24cf
Jinho Yim <jinhoyim@outlook.com>
2024-12-11 02:37:26 +0900

    Hibernate BatchSize 설정 조회 예

---
+++ Ch12/batch/src/main/java/book/manning/javapersistence/ch12/batch/Bid.java
@@ -0,0 +1,66 @@
+package book.manning.javapersistence.ch12.batch;
+
+import book.manning.javapersistence.ch12.Constants;
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+public class Bid {
+
+    @Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Item item;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User bidder;
+
+    @NotNull
+    private BigDecimal amount;
+
+    protected Bid() {}
+
+    public Bid(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Bid(Item item, User bidder, BigDecimal amount) {
+        this.item = item;
+        this.bidder = bidder;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public User getBidder() {
+        return bidder;
+    }
+
+    public void setBidder(User bidder) {
+        this.bidder = bidder;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
