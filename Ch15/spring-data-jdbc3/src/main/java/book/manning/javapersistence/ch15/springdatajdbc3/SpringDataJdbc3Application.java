82a405c980bf932916bd8e47cce7877d2f342943
Jinho Yim <jinhoyim@outlook.com>
2024-12-13 18:57:02 +0900

    Spring Data JDBC 관계 추가를 위한 프로젝트 생성

---
+++ Ch15/spring-data-jdbc3/src/main/java/book/manning/javapersistence/ch15/springdatajdbc3/SpringDataJdbc3Application.java
@@ -0,0 +1,13 @@
+package book.manning.javapersistence.ch15.springdatajdbc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJdbc3Application.class, args);
+    }
+
+}
